d61878612041296ce0a8965c8ffcacc82de3299f
Leonardo <60203599+leonardopollesel@users.noreply.github.com>
2020-05-16 22:05:34 +0200

    up

--- FT_Leonardo_Pollesel_A1.py
+++ FT_Leonardo_Pollesel_A1.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 2*np.pi, 0.01)
-y = np.sin(x)
-z = np.cos(x)
-plt.plot(x, y, x, z)
-plt.title('One period of cos and sin')
-plt.legend(['sin(x)', 'cos(x)'])
-plt.show()
+
+def function():
+    x = np.arange(0, 2*np.pi, 0.01)
+    y = np.sin(x)
+    z = np.cos(x)
+    plt.plot(x, y, x, z)
+    plt.title('One period of cos and sin')
+    plt.legend(['sin(x)', 'cos(x)'])
+    retunr plt.show()
+
+if __name__ == '__main__':
+    function()
